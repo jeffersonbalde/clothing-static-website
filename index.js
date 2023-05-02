82e517198a2451bf0dbc001442de8f2d4700b339
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-03 01:22:39 +0800

    styling product details

--- index.js
+++ index.js
@@ -108,3 +108,31 @@ const swiper = new Swiper('.sliderbox', {
         }
     }
   });
+
+
+// product image > page-single
+const thumbImage = new Swiper('.thumbnail-image', {
+
+    // loop: true, 
+    direction: 'vertical',
+    spaceBetween: 15,
+    slidesPerView: 1,
+    freeMode: true,
+    watchSlidesProgress: true,
+
+  });
+
+  const mainImage = new Swiper('.main-imgage', {
+
+    loop: true, 
+    autoHeight: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    thumbs: {
+        swiper: thumbImage,
+    },
+
+  });
