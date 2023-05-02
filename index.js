f42bbd5a40c8bf239af8abe821fd59d9bf1c61c6
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-02 22:55:10 +0800

    working in footer

--- index.js
+++ index.js
@@ -48,9 +48,25 @@ if(sorter) {
     }))
 }
 
+// tabbed
+const trigger = document.querySelectorAll('.tabbed-trigger'),
+    content = document.querySelectorAll('.tabbed > div');
+
+trigger.forEach((btn) => {
+    btn.addEventListener('click', function() {
+        let dataTarget = this.dataset.id,
+        body = document.querySelector(`#${dataTarget}`);
+
+        trigger.forEach((b) => b.parentNode.classList.remove('active'));
+        content.forEach((s) => s.classList.remove('active'));
+        this.parentNode.classList.add('active');
+        body.classList.add('active');
+    })
+})
+
+
 // slider
 const swiper = new Swiper('.sliderbox', {
-
     loop: true, 
     effect: 'fade',
     autoHeight: true,
